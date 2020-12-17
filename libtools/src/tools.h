948246387a42a73c62d56925e641b417f1320c9d
Webkadiz <webkadiz@gmail.com>
2020-12-17 20:44:04 +0300

    different tools for convenience

--- libtools/src/tools.h
+++ libtools/src/tools.h
@@ -2,14 +2,34 @@
 #define TOOLS_M
 
 #define print_arr(arr, len, temp) \
-for (int i = 0; i < len; i++) { \
-    printf(temp, arr[i]); \
+for (int local_i = 0; local_i < len; local_i++) { \
+    printf(temp, arr[local_i]); \
 } \
+
+#define print_matrix(matrix, rows, cols, temp) \
+for (int local_i = 0; local_i < rows; local_i++) { \
+    for (int local_j = 0; local_j < cols; local_j++ ) { \
+        printf(temp, matrix[local_i][local_j]); \
+    } \
+    printf("\n"); \
+} \
+
+#define free_matrix(matrix, rows) \
+for(int local_i = 0; local_i < rows; local_i++) { \
+    free(matrix[local_i]); \
+} \
+free(matrix); \
 
 #define true 1
 #define false 0
-#define and &&
-#define or ||
-#define not !
+
+#define int8 signed char
+#define int16 signed short
+#define int32 signed int
+#define int64 signed long
+#define uint8 unsigned char
+#define uint16 unsigned short
+#define uint32 unsigned int
+#define uint64 unsigned long
 
 #endif
